2b6e2d7a042fcbdb39bf6fd28965798456c20d44
jamieRollison <rollisonjamie15@gmail.com>
2023-09-09 13:51:00 -0400

    change button in navbar

--- src/components/navbar.tsx
+++ src/components/navbar.tsx
@@ -1,8 +1,11 @@
 import Image from "next/image";
+import { signIn, signOut, useSession } from "next-auth/react";
 
 export default function Navbar() {
+  const { data: session } = useSession();
+
   return (
-    <nav className="bg-teal-500 flex flex-wrap items-center justify-between bg-secondary p-6">
+    <nav className="bg-secondary flex flex-wrap items-center justify-between bg-teal-500 p-6">
       <div className="mr-6 flex flex-shrink-0 items-center text-white">
         <Image
           src="/ss_logo.png"
@@ -12,9 +15,9 @@ export default function Navbar() {
         ></Image>
       </div>
       <div className="block lg:hidden">
-        <button className="text-teal-200 border-teal-400 flex items-center rounded border px-3 py-2 hover:border-white hover:text-white">
+        <button className="flex items-center rounded border border-teal-400 px-3 py-2 text-teal-200 hover:border-white hover:text-white">
           <svg
-            className="fill-current h-3 w-3"
+            className="h-3 w-3 fill-current"
             viewBox="0 0 20 20"
             xmlns="http://www.w3.org/2000/svg"
           >
@@ -24,33 +27,27 @@ export default function Navbar() {
         </button>
       </div>
       <div className="block w-full flex-grow lg:flex lg:w-auto lg:items-center">
-        <div className="text-sm lg:flex-grow sm:space-x-0 lg:space-x-4">
+        <div className="text-sm sm:space-x-0 lg:flex-grow lg:space-x-4">
           <a
             href="about"
-            className="mt-4 block font-mono text-white transition duration-100 ease-in-out hover:text-primary lg:mt-0 lg:inline-block"
+            className="hover:text-primary mt-4 block font-mono text-white transition duration-100 ease-in-out lg:mt-0 lg:inline-block"
           >
             About
           </a>
           <a
             href="edit"
-            className="mt-4 block font-mono text-white transition duration-100 ease-in-out hover:text-primary lg:mt-0 lg:inline-block"
+            className="hover:text-primary mt-4 block font-mono text-white transition duration-100 ease-in-out lg:mt-0 lg:inline-block"
           >
             Edit
           </a>
         </div>
         <div className="space-x-2">
-          <a
-            href="#"
-            className="mt-4 inline-block rounded border border-white px-4 py-2 text-sm leading-none text-white hover:border-primary hover:bg-primary hover:text-white lg:mt-0"
+          <button
+            onClick={session ? () => void signOut() : () => void signIn()}
+            className="hover:border-primary hover:bg-primary mt-4 inline-block rounded border border-white px-4 py-2 text-sm leading-none text-white hover:text-white lg:mt-0"
           >
-            Login
-          </a>
-          <a
-            href="#"
-            className="mt-4 inline-block rounded border border-white px-4 py-2 text-sm leading-none text-white  hover:border-primary hover:bg-primary hover:text-white lg:mt-0"
-          >
-            Sign Up
-          </a>
+            {session ? "Sign out" : "Sign in"}
+          </button>
         </div>
       </div>
     </nav>
