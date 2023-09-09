4e31050b9ea8b43cb75d4e6614ad5d7c84b65a08
Cole Zimmerman <colezimmerman01@gmail.com>
2023-09-09 17:53:03 -0400

    function for calling chatgpt

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,18 +1,18 @@
 import { type Session } from "next-auth";
 import { SessionProvider } from "next-auth/react";
 import { type AppType } from "next/app";
-import { Roboto_Mono } from 'next/font/google'
+import { Roboto_Mono } from "next/font/google";
 
 import { api } from "~/utils/api";
 
 import "~/styles/globals.css";
 
 const roboto_mono = Roboto_Mono({
-  style: ['normal', 'italic'],
-  subsets: ['latin'],
-  weight: ['100', '400', '600'],
-  variable: '--font-roboto-mono',
-})
+  style: ["normal", "italic"],
+  subsets: ["latin"],
+  weight: ["100", "400", "600"],
+  variable: "--font-roboto-mono",
+});
 
 const MyApp: AppType<{ session: Session | null }> = ({
   Component,
@@ -21,7 +21,7 @@ const MyApp: AppType<{ session: Session | null }> = ({
   return (
     <SessionProvider session={session}>
       <main className={`${roboto_mono.variable} font-mono`}>
-      <Component {...pageProps} />
+        <Component {...pageProps} />
       </main>
     </SessionProvider>
   );
--- src/server/api/routers/metaphor.ts
+++ src/server/api/routers/metaphor.ts
@@ -1,9 +1,11 @@
 import { z } from "zod";
 
 import { createTRPCRouter, publicProcedure } from "~/server/api/trpc";
+import OpenAI from "openai";
 
 import { env } from "~/env.mjs";
 import sdk from "@api/metaphorapi";
+import { CreateChatCompletionRequestMessage } from "openai/resources/chat";
 
 export const metaphorRouter = createTRPCRouter({
   search: publicProcedure.input(z.string()).query(async ({ input }) => {
@@ -23,4 +25,27 @@ export const metaphorRouter = createTRPCRouter({
         return [];
       });
   }),
-});
+  askgpt: publicProcedure.input(z.string()).query(async ({ input }) => {
+    const openai = new OpenAI();
+      const messages = [
+        {
+          role: "user",
+          content: `consider the following paragraph 
+    and make a list of 2-tuples in javascript without assigning it to a variable where 
+    the first value in each tuple is a string with a word / concept / proper noun that
+     may need to be defined, and the second value is a string with the first entry's definition. 
+     ignore words which are common or semantically neutral and if something seems like a proper noun, include it.`,
+        },
+        {
+          role: "user",
+          content: input,
+        },
+      ] as CreateChatCompletionRequestMessage[];
+
+      const response = await openai.chat.completions.create({
+        model: "gpt-3.5-turbo",
+        messages: messages,
+    });
+    const responseMessage = [];
+    })
+  });
