66067ecf2843cc4a207cc632f3c1b0bac8a07da1
Owen Cushing <opcushing@gmail.com>
2023-09-08 23:28:31 -0400

    made header

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -1,9 +1,22 @@
 import { type Config } from "tailwindcss";
 
+import colors from "tailwindcss/colors";
+
+
 export default {
   content: ["./src/**/*.{js,ts,jsx,tsx}"],
   theme: {
     extend: {},
+    colors: {
+      'dark': '#2E3141',
+      'primary': '#E1885E',
+      'primary_dark': '#D66A37', 
+      'secondary': '#4F5D75',
+      'gray': '#BFC0C0', 
+      'white': '#FFFFFF', 
+      'black': '#1E1E1E',
+      'sky': colors.sky,
+    },
   },
   plugins: [],
 } satisfies Config;
