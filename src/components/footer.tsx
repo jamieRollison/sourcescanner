dff977a06268405ca6f8c46c95477562e9cf39ad
estrella <epopo2@illinois.edu>
2023-09-10 00:31:39 -0400

    add images and about me page

--- src/components/footer.tsx
+++ src/components/footer.tsx
@@ -6,9 +6,14 @@ export default function Navbar() {
     // fixed bottom-0 left-0
     <footer className="flex w-full flex-wrap items-center justify-between bg-secondary p-4">
       <div className="flex items-center">
-      <Image className="mr-6"alt="" src={UIUC} width={30} height={30} />
-     
-      <p className="text-white">Made with ♡ by <span className="underline">Jamie</span>, <span className="underline">Cole</span>, <span className="underline">Ellie</span>, and <span className="underline">Owen</span> @ Pennapps, 2023</p>
+        <Image className="mr-6" alt="" src={UIUC} width={30} height={30} />
+
+        <p className="text-white">
+          Made with ♡ by <span className="underline">Jamie</span>,{" "}
+          <span className="underline">Cole</span>,{" "}
+          <span className="underline">Ellie</span>, and{" "}
+          <span className="underline">Owen</span> @ Pennapps, 2023
+        </p>
       </div>
       <a href="https://github.com/jamieRollison/sourcescanner">
         <Image
